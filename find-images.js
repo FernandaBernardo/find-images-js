81fcf5a46a92345bfc0bc22c597d7acc2be539cb
Fernanda Bernardo <fernandabernardo94@gmail.com>
2017-08-03 22:28:09 -0300

    pega imagens da segunda página, totalizando 40 imagens

--- find-images.js
+++ find-images.js
@@ -2,21 +2,30 @@ const request = require('request'),
 	cheerio = require('cheerio'),
 	shell = require('shelljs');
 
+const googleUrl = "https://www.google.com.br";
 
 let query = "arroz";
-shell.mkdir("-p", "food-images/" + query);
+shell.mkdir("-p", `food-images/${query}`);
 
-request('https://www.google.com.br/search?tbm=isch&q=' + query, function (error, response, html) {
+let downloadImagesFromHtml = function($, start) {
+	$('.images_table img').each(function(i, element){
+		shell.exec(`curl ${element.attribs.src} > food-images/${query}/image${i + start}.png`);
+	});
+}
+
+request(googleUrl + '/search?tbm=isch&q=' + query, function (error, response, html) {
+	let $ = cheerio.load(html);
 	if (!error && response.statusCode == 200) {
-		var $ = cheerio.load(html);
-		$('.images_table img').each(function(i, element){
-			shell.exec("curl " + element.attribs.src + " > food-images/" + query + "/image" + i + ".png")
-		});
+		downloadImagesFromHtml($, 0);
 
-		// pegar o próximo link
-		// $('.b:last-child .fl').each(function(i, element) {
-		// 	console.log("link", element.attribs.href);
-		// });
+		$('.b:last-child .fl').each(function(i, element) {
+			request(googleUrl + element.attribs.href, function(error, response, html) {
+				$ = cheerio.load(html);
+				if (!error && response.statusCode == 200) {
+					downloadImagesFromHtml($, 20);
+				}
+			});
+		});
 	}
 });
 
